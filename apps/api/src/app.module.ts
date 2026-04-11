44d17885d7b98d0951ddf1d941b08e3a59f804b8
Davi Duarte <rio.daviduarte@gmail.com>
2026-04-11 01:32:29 -0300

    feat(api): add PrismaService as global module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './common/prisma/prisma.module.js';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
