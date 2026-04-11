fc04e4277a123db77dddbc721bc8603ce47b7811
Davi Duarte <rio.daviduarte@gmail.com>
2026-04-11 02:57:02 -0300

    feat(api): add GoogleCalendarService with free/busy + event CRUD

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -13,6 +13,7 @@ import { UsersModule } from './users/users.module.js';
 import { CyclesModule } from './cycles/cycles.module.js';
 import { PrivacyModule } from './privacy/privacy.module.js';
 import { LibraryModule } from './library/library.module.js';
+import { GoogleCalendarModule } from './google-calendar/google-calendar.module.js';
 import { loadEnv } from './config/env.js';
 
 @Module({
@@ -44,6 +45,7 @@ import { loadEnv } from './config/env.js';
     CyclesModule,
     PrivacyModule,
     LibraryModule,
+    GoogleCalendarModule,
     HealthModule,
   ],
   providers: [
