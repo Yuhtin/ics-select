35a0f3d8120de3d7215ffbbb7a9222b8e071dc83
Davi Duarte <rio.daviduarte@gmail.com>
2026-04-20 01:24:10 -0300

    feat(api): add /me/calendar controller (GET week, PATCH reschedule)

--- apps/api/src/me/calendar/calendar.module.ts
+++ apps/api/src/me/calendar/calendar.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { GoogleCalendarModule } from '../../google-calendar/google-calendar.module.js';
 import { MeCalendarService } from './calendar.service.js';
+import { MeCalendarController } from './calendar.controller.js';
 
 @Module({
+  imports: [GoogleCalendarModule],
+  controllers: [MeCalendarController],
   providers: [MeCalendarService],
   exports: [MeCalendarService],
 })
